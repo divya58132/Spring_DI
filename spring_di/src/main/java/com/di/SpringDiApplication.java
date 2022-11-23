4abf854ead665d8eef0a0c8d40e7bac110fbd1e1
divya58132 <divyabansal58132@gmail.com>
2022-11-23 21:48:40 +0530

    profiles

--- spring_di/src/main/java/com/di/SpringDiApplication.java
+++ spring_di/src/main/java/com/di/SpringDiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import com.di.controllers.ConstructorInjectorController;
+import com.di.controllers.I18nController;
 import com.di.controllers.MyController;
 import com.di.controllers.PropertyInjectorController;
 import com.di.controllers.SetterInjectorController;
@@ -15,15 +16,23 @@ public class SpringDiApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx=SpringApplication.run(SpringDiApplication.class, args);
 		MyController myController = (MyController) ctx.getBean("myController");
+		I18nController i18nController=(I18nController) ctx.getBean("i18nController");
+		System.out.println("_______Dependency with spring_______");
+		
+		System.out.println("------Profiles------");
+		System.out.println(i18nController.sayHello());
+		
 		System.out.println("------Primary Bean------");
 		System.out.println(myController.sayHello());
-		System.out.println("_______Dependency with spring_______");
+		
 		System.out.println("------Property Injector------");
 		PropertyInjectorController propertyInjectorController=(PropertyInjectorController) ctx.getBean("propertyInjectorController");
 		System.out.println(propertyInjectorController.getGreeting());
+		
 		System.out.println("------Setter Injector------");
 		SetterInjectorController setterInjectorController=(SetterInjectorController) ctx.getBean("setterInjectorController");
 		System.out.println(setterInjectorController.getGreeting());
+		
 		System.out.println("------Constructor Injector------");
 		ConstructorInjectorController constructorInjectorController=(ConstructorInjectorController) ctx.getBean("constructorInjectorController");
 		System.out.println(constructorInjectorController.getGreeting());
---
+++ spring_di/src/main/java/com/di/services/I18nSpanishService.java
@@ -0,0 +1,16 @@
+package com.di.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("SP")
+@Service("i18nService")
+public class I18nSpanishService implements GreetingService{
+
+	@Override
+	public String sayGreeting() {
+		// TODO Auto-generated method stub
+		return "Greeting - Spanish";
+	}
+
+}
