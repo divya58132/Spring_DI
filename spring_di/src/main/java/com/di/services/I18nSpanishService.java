24ba1441a40a32a0e06c003ac2a50e9837b17465
divya58132 <divyabansal58132@gmail.com>
2022-12-26 16:23:17 +0530

    default profile

--- spring_di/src/main/java/com/di/services/I18nSpanishService.java
+++ spring_di/src/main/java/com/di/services/I18nSpanishService.java
@@ -3,7 +3,7 @@ package com.di.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("SP")
+@Profile({"SP","default"})
 @Service("i18nService")
 public class I18nSpanishService implements GreetingService{
 
